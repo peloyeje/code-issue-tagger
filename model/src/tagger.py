537f90e9c2c8fef507061114cf58b40a0ff247ad
sebastian schwarz <sebastian.ca.schwarz@gmail.com>
2018-10-18 16:18:19 +0200

    added some error handling

--- model/src/tagger.py
+++ model/src/tagger.py
@@ -4,35 +4,25 @@ from embedder import embedder
 
 class tagger:
 
-	def __init__(self, string, trained_model, clean_string = None, status = None, net = None):
+	def __init__(self, string, trained_model, clean_string = None, status = 'raw', model = None):
 		self.string = string
 		self.model =  None
 		self.trained_model = trained_model
 		self.clean_string = clean_string
-		self.status = 'raw'
+		self.status = status
 
 	def _initialize(self):
 		if self.model is None :
 			self.model = nn.Module()
 
-	def _loadmodel(self):
-		if not self._initialized:
-			self._initialize()
-		else:
-			if isinstance(trained_model, nn.Module):
-				self.model._load_state_dict(trained_model)
-				self.model.eval()
-			else:
-				self.model = None
-
-	@property
-	def _validstring(self):
-		return isinstance(self.string, str):
-
 	@property
 	def _initialized(self):
 		return self._model is not None
 
+	@property
+	def _validstring(self):
+		return isinstance(self.string, str):
+	
 	def clean(self):
 		if self._validstring:
 			self.clean_string =  extract_text_content(self.string)
@@ -52,13 +42,19 @@ class tagger:
 			self.embedded = embedd(PATH, self.clean_string)
 			self.status = 'embedded'
 
+	def loadmodel(self):
+	try:
+		self._initialize()
+	except:
+		print('could not initialize model')
+	
+	if self._initialized and isinstance(trained_model, nn.Module):
+		self.model._load_state_dict(trained_model)
+		self.model.eval()
+	else:
+		self.model = None
+		print('could not load model')
+
 	def predict(self):
 		if self.status = 'embedded' and self.model is not None:
 			return self.model(self.embedded)
-
-
-
-
-
-
-
