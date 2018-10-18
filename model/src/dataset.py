5ee9ebd25adbb311a3b1b30a80a9711b223a54e2
Jean-Eudes Peloye <hello@peloye.je>
2018-10-18 21:58:31 +0200

    add column subsetting in dataset

--- model/src/dataset.py
+++ model/src/dataset.py
@@ -20,16 +20,17 @@ class Dataset(data.Dataset):
         else:
             self.__dict__.update(files)
 
-        self._dataset = self._open_dataset(self._dataset)
+        self._dataset = self._open_dataset(self._dataset, ['body', 'tags'])
         self._embedder = Embedder(vocabulary=self._vocabulary,
                                   tags=self._tags)
 
     def set_options(self, max_length=250):
         self.max_length = max_length
 
-    def _open_dataset(self, path):
+    def _open_dataset(self, path, columns=None):
         try:
-            return pd.read_csv(path, index_col=0).dropna()
+            df = pd.read_csv(path, index_col=0).dropna()
+            return df[columns] if columns else df
         except Exception as e:
             raise e
 
@@ -37,5 +38,5 @@ class Dataset(data.Dataset):
         return len(self._dataset)
 
     def __getitem__(self, index):
-        X, y = self._dataset.iloc[index, [1, 2]]
+        X, y = self._dataset.iloc[index, [0, 1]]
         return self._embedder.embed(X, y, pad=True, pad_length=self.max_length)
