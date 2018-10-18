933f7f98a65e2d6ee3384f850d7c718b60e21663
sebastian schwarz <sebastian.ca.schwarz@gmail.com>
2018-10-18 15:59:18 +0200

    tagger class for input prediction

---
+++ model/src/tagger.py
@@ -0,0 +1,64 @@
+import torch.nn as nn
+from utils import *
+from embedder import embedder 
+
+class tagger:
+
+	def __init__(self, string, trained_model, clean_string = None, status = None, net = None):
+		self.string = string
+		self.model =  None
+		self.trained_model = trained_model
+		self.clean_string = clean_string
+		self.status = 'raw'
+
+	def _initialize(self):
+		if self.model is None :
+			self.model = nn.Module()
+
+	def _loadmodel(self):
+		if not self._initialized:
+			self._initialize()
+		else:
+			if isinstance(trained_model, nn.Module):
+				self.model._load_state_dict(trained_model)
+				self.model.eval()
+			else:
+				self.model = None
+
+	@property
+	def _validstring(self):
+		return isinstance(self.string, str):
+
+	@property
+	def _initialized(self):
+		return self._model is not None
+
+	def clean(self):
+		if self._validstring:
+			self.clean_string =  extract_text_content(self.string)
+			self.status = 'clean'
+
+	def prepropress(self):
+		if self.status == 'clean':
+			self.clean_string = preprocess_string(self.clean_string)
+			self.status = 'preprocessed'
+		else: 
+			self.clean()
+			self.clean_string = preprocess_string(self.clean_string)
+			self.status = 'preprocessed'
+
+	def embed(self)
+		if self.status == 'preprocessed':
+			self.embedded = embedd(PATH, self.clean_string)
+			self.status = 'embedded'
+
+	def predict(self):
+		if self.status = 'embedded' and self.model is not None:
+			return self.model(self.embedded)
+
+
+
+
+
+
+
